f6d9b322f520bf91801679ac0135f5f6621bd79d
carlo briganti <505641nl@saeinstitute.edu>
2020-09-02 12:44:27 +0200

    milestone 1

---
+++ js/app.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  var titolo = 'pippo';
+  $.ajax({
+    url: "https://api.themoviedb.org/3/search/movie",
+    method: "GET",
+    data : {
+        api_key : "911c39a0e26befb8fde97cb0a1c177cd",
+        query: titolo,
+        language: 'it-IT'
+    },
+    success: function (risposta){
+      compileHandlebar(risposta.results);
+    },
+    error: function (){
+        alert('Si e verificato un errore :( riprova piu tardi');
+    },
+  });
+});
+function compileHandlebar(risp){
+  for (i=0;i<risp.length;i++){
+   var source = $('#movie_container').html();
+   var template = Handlebars.compile(source);
+   var context = {
+    titolo: risp[i].title,
+    titoloOriginale: risp[i].original_title,
+    lang: risp[i].original_language,
+    rating: risp[i].vote_average
+   }
+   var html = template(context);
+   $('.container').append(html);
+  }
+}
