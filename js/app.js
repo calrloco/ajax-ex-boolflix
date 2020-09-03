278687836174e6bf7ed0ac54d7256f3a6bf24a62
carlo briganti <505641nl@saeinstitute.edu>
2020-09-03 12:52:18 +0200

    flags e starts

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
   // animazione poput e chiusura input al click della lente
   popSearch();
   // funzione per cercare in automatico mentre scrivi
-  $(".search_movie").keyup(function () {
+  $(".search_movie").keydown(function () {
     var urlSearch = "https://api.themoviedb.org/3/search/movie";
     var titolo = $(".search_movie").val();
     if ($(this).val().length != 0) {
@@ -13,6 +13,8 @@ $(document).ready(function () {
       trendMoviesHomePage();
     }
   });
+  /// film popolari al click del tasto trend mocies sulla nav;
+  $(".trending").click(trendMoviesHomePage);
 });
 // funzione ricerca Api che richiama la funzione che compila il template
 function cercaFilm(titolo, url) {
@@ -33,6 +35,7 @@ function cercaFilm(titolo, url) {
     success: function (risposta) {
       // se ci sono risposte fa la funzione
       if (risposta.results.length != 0) {
+        console.log(risposta.results);
         compileHandlebar(risposta.results);
       } else {
         $(".not-found").text(
@@ -54,12 +57,13 @@ function compileHandlebar(risp) {
   for (i = 0; i < risp.length; i++) {
     var source = $("#movie_container").html();
     var template = Handlebars.compile(source);
-    var rating = Math.round(risp[i].vote_average / 2);
+    var rating = risp[i].vote_average;
+    var lingua =  risp[i].original_language;
     var context = {
       titolo: risp[i].title,
       titoloOriginale: risp[i].original_title,
-      lang: risp[i].original_language,
-      rating: rating,
+      lang: nationFlag(lingua),
+      rating: addStar(rating),
     };
     var html = template(context);
     $(".container").append(html);
@@ -71,30 +75,51 @@ function trendMoviesHomePage() {
   var titolo = "";
   cercaFilm(titolo, PopularThisWeek);
 }
+function addStar(rat) {
+  var voto = Math.ceil(rat / 2);
+  var stars = '';
+  for (var i = 0; i < 5; i++) {
+    if(i<=voto){
+      var star = '<i class="fas fa-star rating yellow"></i>';
+    }else{
+      var star = '<i class="fas fa-star rating"></i>';
+    }
+    stars += star;
+  }
+  console.log(i);
+  return stars;
+}
+function nationFlag(lingua){
+    if(lingua == 'en'){
+        lingua =  "<img src='https://svgshare.com/i/PH_.svg' alt=\"UK-flag\" class=\"flags\" title='' />";
+    }else if(lingua == 'it'){
+      lingua = "<img src='https://svgshare.com/i/PFJ.svg' alt=\"IT-flag\" class=\"flags\" title=''/>"
+    }
+    return lingua
+  }
 ////////// animazioni//////////////////////////
 function popSearch() {
   $(".search_movie__btn").click(function () {
     $(".search_movie").toggleClass("search-full");
-    if($(this).hasClass('fa-times')){
+    if ($(this).hasClass("fa-times")) {
       trendMoviesHomePage();
-      $(".search_movie").val('');
+      $(".search_movie").val("");
     }
     if ($(this).hasClass("fa-search") == true) {
       $(this).removeClass("fa-search");
       $(this).addClass("fa-times");
-      $('.search_movie').focus();
+      $(".search_movie").focus();
     } else {
       $(this).addClass("fa-search");
       $(this).removeClass("fa-times");
     }
   });
-  //////scroll 
-  $(window).bind('mousewheel', function(event) {
+  //////scroll cabio bg-color header///////////
+  $(window).bind("mousewheel", function (event) {
     if (event.originalEvent.wheelDelta >= 0) {
-      $('.header').removeClass('header-darker');
+      $(".header").removeClass("header-darker");
+    } else {
+      $(".header").addClass("header-darker");
     }
-    else {
-      $('.header').addClass('header-darker');
-    }
-});
+  });
 }
